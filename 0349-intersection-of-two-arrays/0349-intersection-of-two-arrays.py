08c8d22d4befc24d867b74f714523bea8c8144a1
kambojmayan-png <kambojmayan@gmail.com>
2026-08-02 22:50:19 +0530

    Time: 0 ms (100%), Space: 12.6 MB (30.64%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -2,5 +2,4 @@ class Solution(object):
     def intersection(self, nums1, nums2):
         set1 = set(nums1)
         set2 = set(nums2)
-        result = list(set1 & set2)
-        return result
+        return list(set1 & set2)
