d666130c21491bc3d988afee2799256fd80e16bc
kambojmayan-png <kambojmayan@gmail.com>
2026-08-02 22:48:10 +0530

    Time: 0 ms (100%), Space: 12.6 MB (30.64%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,15 +1,6 @@
 class Solution(object):
     def intersection(self, nums1, nums2):
-        common = set()
-        if (len(nums1) > len(nums2)):
-            for i in nums2:
-                if i in nums1:
-                    common.add(i)
-        else:
-            for i in nums1:
-                if i in nums2:
-                    common.add(i)
-        common_list = []
-        for i in common:
-            common_list.append(i)
-        return common_list
+        set1 = set(nums1)
+        set2 = set(nums2)
+        result = list(set1 & set2)
+        return result
