f0605ac183a3e0dcf0272df24b70eb7de0dc6110
kambojmayan-png <kambojmayan@gmail.com>
2026-08-03 22:07:09 +0530

    Time: 0 ms (100%), Space: 19.7 MB (76.79%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,10 +1,8 @@
 class Solution(object):
     def reverseString(self, s):
         l,r = 0,len(s)-1
-        for i in range(len(s)/2):
-            temp = s[l]
-            s[l] = s[r]
-            s[r] = temp
+        while l < r:
+            s[l],s[r] = s[r],s[l]
             l += 1
             r -= 1
         return s
