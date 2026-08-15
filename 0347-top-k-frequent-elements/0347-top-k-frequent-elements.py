387e37f956f184efadf20cf2e7950d2729c1616e
kambojmayan-png <kambojmayan@gmail.com>
2026-08-15 22:48:09 +0530

    Time: 134 ms (5.22%), Space: 22.9 MB (41.67%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,22 @@
+from collections import Counter
+from typing import List
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+     
+        freqMap = Counter(nums)
+       
+        arr = list(freqMap.items())
+       
+        for i in range(len(arr)):
+            index = i
+            for j in range(i + 1, len(arr)):
+                if arr[j][1] > arr[index][1]:   
+                    index = j
+            arr[i], arr[index] = arr[index], arr[i]  
+  
+        final = []
+        for i in range(k):
+            final.append(arr[i][0])  
+
+        return final
