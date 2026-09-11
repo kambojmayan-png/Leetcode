6ea811e1e3213162569c093b172729b0d91e0ac6
kambojmayan-png <kambojmayan@gmail.com>
2026-09-11 23:03:51 +0530

    Time: 15 ms (35.47%), Space: 13.4 MB (86.95%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def trap(self, height):
-        if len(height) == 0:
-            return 0
+
+        if not height: return 0
 
         l,r = 0,len(height)-1
         Max_left = height[l]
@@ -12,16 +12,12 @@ class Solution(object):
         while l < r:
             if height[l] <= height[r]:
                 l += 1
-                if min(Max_left,Max_right) - height[l] > 0:
-                    res += min(Max_left,Max_right) - height[l]
-
                 Max_left = max(height[l],Max_left)
-            
+                res += Max_left - height[l]
+  
             else:
                 r -= 1
-                if min(Max_left,Max_right) - height[r] > 0:
-                    res += min(Max_left,Max_right) - height[r]
-                
                 Max_right= max(height[r],Max_right)
-
+                res += Max_right - height[r]
+                
         return res
